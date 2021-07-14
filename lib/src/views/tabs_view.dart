edc9afb19afd2a8aa58c30e7430e1799a4c22622
Sebastian Otalora <jhonsebas77@outlook.com>
2021-07-14 14:55:27 -0500

    Add the bottom view

--- lib/src/views/tabs_view.dart
+++ lib/src/views/tabs_view.dart
@@ -5,18 +5,53 @@ class TabsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: PageView(
-        physics: BouncingScrollPhysics(),
-        children: [
-          Container(
-            color: Colors.red,
-          ),
-          Container(
-            color: Colors.green,
-          )
-        ],
-      ),
+    return const Scaffold(
+      body: _BuildPages(),
+      bottomNavigationBar: _BuildNavigation(),
+    );
+  }
+}
+
+class _BuildNavigation extends StatelessWidget {
+  const _BuildNavigation({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: 0,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'For You',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.public),
+          label: 'Popular',
+        ),
+      ],
+    );
+  }
+}
+
+class _BuildPages extends StatelessWidget {
+  const _BuildPages({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.green,
+        )
+      ],
     );
   }
 }
