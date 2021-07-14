695f8310d35030c7c291490bd5894f2c0b4cffc1
Sebastian Otalora <jhonsebas77@outlook.com>
2021-07-14 14:41:49 -0500

    Create proyect

---
+++ android/app/src/main/kotlin/com/example/providers_news/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.providers_news
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/views/tabs_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TabsView extends StatelessWidget {
+  const TabsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('d'),
+      ),
+    );
+  }
+}
