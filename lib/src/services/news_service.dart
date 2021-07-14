c24c52631f06e4db6de3c5988731304fd124a1bb
Sebastian Otalora <jhonsebas77@outlook.com>
2021-07-14 16:05:55 -0500

    Make the request for News

--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:providers_news/src/models/news_models.dart';
+
+const _basePathNews = 'newsapi.org';
+const _apiKey = '9147785630d74507b23a50239aedbc4a';
 
 class NewServices extends ChangeNotifier {
   List<Article> headlines = [];
@@ -7,7 +11,18 @@ class NewServices extends ChangeNotifier {
     getTopHeadlines();
   }
 
-  getTopHeadlines() {
-    print('Loading...');
+  getTopHeadlines() async {
+    final url = Uri.https(
+      _basePathNews,
+      '/v2/top-headlines',
+      {
+        'apiKey': _apiKey,
+        'country': 'co',
+      },
+    );
+    final response = await http.get(url);
+    final newResponse = newResponseFromJson(response.body);
+    headlines.addAll(newResponse.articles);
+    notifyListeners();
   }
 }
