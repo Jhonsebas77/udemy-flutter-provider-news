c46dd4926f33775960bfff425cda032955a49fc7
Sebastian Otalora <jhonsebas77@outlook.com>
2021-07-15 10:29:29 -0500

    Add the categories

--- lib/src/services/news_service.dart
+++ lib/src/services/news_service.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
+import 'package:providers_news/src/models/category_model.dart';
 import 'package:providers_news/src/models/news_models.dart';
 
 const _basePathNews = 'newsapi.org';
@@ -7,6 +9,16 @@ const _apiKey = '9147785630d74507b23a50239aedbc4a';
 
 class NewServices extends ChangeNotifier {
   List<Article> headlines = [];
+  List<Category> categories = [
+    Category(FontAwesomeIcons.building, 'bussines'),
+    Category(FontAwesomeIcons.tv, 'entertainment'),
+    Category(FontAwesomeIcons.addressCard, 'general'),
+    Category(FontAwesomeIcons.headSideVirus, 'health'),
+    Category(FontAwesomeIcons.vials, 'science'),
+    Category(FontAwesomeIcons.volleyballBall, 'sports'),
+    Category(FontAwesomeIcons.memory, 'technology'),
+  ];
+
   NewServices() {
     getTopHeadlines();
   }
--- lib/src/views/tabs2_view.dart
+++ lib/src/views/tabs2_view.dart
@@ -1,12 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:providers_news/src/services/news_service.dart';
 
 class Tab2Pages extends StatelessWidget {
   const Tab2Pages({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('HOLA<'),
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: const [
+            Expanded(
+              child: ListCategories(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ListCategories extends StatelessWidget {
+  const ListCategories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final categories = Provider.of<NewServices>(context).categories;
+    return ListView.builder(
+      itemCount: categories.length,
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (_, int index) {
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              Icon(categories[index].icon),
+              const SizedBox(height: 5),
+              Text(categories[index].name),
+            ],
+          ),
+        );
+      },
     );
   }
 }
